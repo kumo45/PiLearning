5c3a3214904ffac15276727dbb062a5ec2d26027
Maciej Ragus <maciekragus@gmail.com>
2022-01-22 00:27:58 +0000

    extracetd first test to separate function; added function for new experiment

--- SimpleDiodePlusButton/main.c
+++ SimpleDiodePlusButton/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <math.h>
 #include <unistd.h>
 
 #include <pigpio.h>
@@ -13,6 +14,14 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    RunDiodeAndButtonTest();
+    RunPulsingDiode();
+    gpioTerminate();
+    return 0;
+}
+
+void RunDiodeAndButtonTest()
+{
     gpioSetMode(4, PI_OUTPUT);
     gpioSetPullUpDown(17,PI_PUD_UP);
     while(1)
@@ -23,7 +32,29 @@ int main(int argc, char *argv[])
         printf("Button state is %d\n", buttonState);  
         usleep(10000);
     }
-
-    gpioTerminate();
-    return 0;
 }
+
+void RunPulsingDiode()
+{
+    gpioSetMode(4, PI_OUTPUT);
+    gpioSetMode(16, PI_INPUT);
+    int antiEpilepsyQuotient = 1000;
+    int counter = 0;
+    int currentState = 0;
+    while(1)
+    {          
+        if(gpioRead(16) == currentState)
+        {
+            counter++;
+        }
+        if(counter == antiEpilepsyQuotient)
+        {
+            counter = 0;
+            currentState = ++currentState%2;
+            gpioWrite(4, currentState);
+        }
+        usleep(110);
+    }
+}
+
+//TODO add async read from stdin that will gracefully shutdown app
